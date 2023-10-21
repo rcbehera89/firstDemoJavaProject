9f8983d5ad14d5d4e7ebddc3d5736b1568052660
The Intution Admin <admin@theintuit.co.in>
2023-10-21 20:18:24 +0530

    added initial commit

---
+++ src/test/java/com/democompany/sample_first_rcproject/AppTest.java
@@ -0,0 +1,20 @@
+	package com.democompany.sample_first_rcproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
